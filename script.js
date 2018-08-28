ef2b5aa8b1fe7fd9b3e0639915b7afef439ebb45
AnastasiaSv <saa@rsamail.ru>
2018-08-28 22:17:26 +0700

    tabs

--- script.js
+++ script.js
@@ -18,21 +18,21 @@ searchAddedFriend.addEventListener('input', () => {
 })
 
 function getFriend(array, input) {
-		for (let i = 0; i < array.length; i++) {
-				if (isMatching(array[i].textContent, input.value)) {
-						array[i].parentNode.style.display = 'flex';
-				} else if (!(isMatching(array[i].textContent, input.value))) {
-						array[i].parentNode.style.display = 'none';
-				}
-		}
+    for (let i = 0; i < array.length; i++) {
+        if (isMatching(array[i].textContent, input.value)) {
+            array[i].parentNode.style.display = 'flex';
+        } else if (!(isMatching(array[i].textContent, input.value))) {
+            array[i].parentNode.style.display = 'none';
+        }
+    }
 }
 
 function isMatching(full, chunk) {
-		if (full.toLowerCase().indexOf(chunk.toLowerCase()) !== -1) {
-				return true;
-		} else {
-				return false;
-		}
+    if (full.toLowerCase().indexOf(chunk.toLowerCase()) !== -1) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 // DnD
@@ -43,68 +43,68 @@ const addedFriendList = document.querySelector('#friends-list ul');
 let currentDrag;
 
 document.addEventListener('dragstart', (e) => {
-		const zone = getCurrentZone(e.target);
+    const zone = getCurrentZone(e.target);
 
-		if (zone) {
-				currentDrag = { startZone: zone, node: e.target };
-		}
+    if (zone) {
+        currentDrag = { startZone: zone, node: e.target };
+    }
 });
 
 document.addEventListener('dragover', (e) => {
-		const zone = getCurrentZone(e.target);
+    const zone = getCurrentZone(e.target);
 
-		if (zone) {
-				e.preventDefault();
-		}
+    if (zone) {
+        e.preventDefault();
+    }
 });
 
 document.addEventListener('drop', (e) => {
-		if (currentDrag) {
-				const zone = getCurrentZone(e.target);
+    if (currentDrag) {
+        const zone = getCurrentZone(e.target);
 
-				e.preventDefault();
+        e.preventDefault();
 
-				if (zone && currentDrag.startZone !== zone) {
-						if (e.target.classList.contains('item')) {
-								zone.insertBefore(currentDrag.node, e.target.nextElementSibling);
-						} else {
-								zone.insertBefore(currentDrag.node, zone.lastElementChild);
-						}
-						let icon = currentDrag.node.querySelector('.fa');
+        if (zone && currentDrag.startZone !== zone) {
+            if (e.target.classList.contains('item')) {
+                zone.insertBefore(currentDrag.node, e.target.nextElementSibling);
+            } else {
+                zone.insertBefore(currentDrag.node, zone.lastElementChild);
+            }
+            let icon = currentDrag.node.querySelector('.fa');
 
-						if (icon.getAttribute('class') == 'fa fa-plus') {
-							icon.setAttribute('class', 'fa fa-remove');
-						} else {
-							icon.setAttribute('class', 'fa fa-plus');
-						}
-				}
+            if (icon.getAttribute('class') == 'fa fa-plus') {
+                icon.setAttribute('class', 'fa fa-remove');
+            } else {
+                icon.setAttribute('class', 'fa fa-plus');
+            }
+        }
 
-				currentDrag = null;
-		}
+        currentDrag = null;
+    }
 });
 
 function getCurrentZone(from) {
-		do {
-				if (from.classList.contains('drop-zone')) {
-						return from;
-				}
-		} while (from = from.parentElement);
+    do {
+        if (from.classList.contains('drop-zone')) {
+            return from;
+        }
+    } while (from = from.parentElement);
 
-		return null;
+    return null;
 }
 
 friendList.addEventListener('click', (e) => {
-		if (e.target.getAttribute('class') == 'fa fa-plus') {
-				e.target.setAttribute('class', 'fa fa-remove');
-				addedFriendList.appendChild(e.target.parentNode);
-		}
+    if (e.target.getAttribute('class') == 'fa fa-plus') {
+        e.target.setAttribute('class', 'fa fa-remove');
+        addedFriendList.appendChild(e.target.parentNode);
+    }
 });
 
 addedFriendList.addEventListener('click', (e) => {
-		if (e.target.getAttribute('class') == 'fa fa-remove') {
-				e.target.setAttribute('class', 'fa fa-plus');
-				friendList.appendChild(e.target.parentNode);
-		}
+    if (e.target.getAttribute('class') == 'fa fa-remove') {
+    e.target.setAttribute('class', 'fa fa-plus');
+        friendList.appendChild(e.target.parentNode);
+    }
 });
 
 // LocalStorage
@@ -113,68 +113,68 @@ const button = document.querySelector('#save');
 let storage = localStorage;
 
 save.addEventListener('click', function() {
-		var list = addedFriends.querySelectorAll('.item');
-		var ids = [];
+    var list = addedFriends.querySelectorAll('.item');
+    var ids = [];
 
-		for (var item of list) {
-				ids.push(item.getAttribute('id'));
-		}
-		storage.data = JSON.stringify(ids);
+    for (var item of list) {
+        ids.push(item.getAttribute('id'));
+    }
+    storage.data = JSON.stringify(ids);
 })
 
 // VK API
 
 VK.init({
-		apiId: 6674957
+    apiId: 6674957
 })
 
 function auth() {
-		return new Promise((resolve, reject) => {
-				VK.Auth.login(data => {
-						if (data.session) {
-								resolve();
-						} else {
-								reject(new Error('Не удалось авторизоваться'));
-						}
-				}, 2)
-		});
+    return new Promise((resolve, reject) => {
+        VK.Auth.login(data => {
+            if (data.session) {
+                resolve();
+            } else {
+                reject(new Error('Не удалось авторизоваться'));
+            }
+        }, 2)
+    });
 }
 
 function callAPI(method, params) {
-		params.v = '5.76';
+    params.v = '5.76';
 
-		return new Promise((resolve, reject) => {
-				VK.api(method, params, (data) => {
-						if (data.error) {
-								reject(data.error);
-						} else {
-								resolve(data.response);
-						}
-				});
-		})
+    return new Promise((resolve, reject) => {
+        VK.api(method, params, (data) => {
+            if (data.error) {
+                reject(data.error);
+            } else {
+                resolve(data.response);
+            }
+        });
+    })
 }
 
 auth()
-		.then(() => {
-				return callAPI('friends.get', { fields: 'photo_50' });
-		})
-		.then(friends => {
-				const template = document.querySelector('#user-template').textContent;
-				const render = Handlebars.compile(template);
-				const html = render(friends);
+    .then(() => {
+        return callAPI('friends.get', { fields: 'photo_50' });
+    })
+    .then(friends => {
+        const template = document.querySelector('#user-template').textContent;
+        const render = Handlebars.compile(template);
+        const html = render(friends);
 
-				friendList.innerHTML = html;
+        friendList.innerHTML = html;
 
-				return new Promise((resolve) => {
-						if (storage.data){
-								var loadedData = JSON.parse(storage.data);
-								resolve(loadedData);
-						}
-				});
-		})
-		.then(loadedData => {
-				for (var item of loadedData) {
-						document.getElementById(item).lastElementChild.setAttribute('class', 'fa fa-remove');
-						addedFriendList.appendChild(document.getElementById(item));
-				}
-		})
+        return new Promise((resolve) => {
+            if (storage.data){
+                var loadedData = JSON.parse(storage.data);
+                resolve(loadedData);
+            }
+        });
+    })
+    .then(loadedData => {
+        for (var item of loadedData) {
+            document.getElementById(item).lastElementChild.setAttribute('class', 'fa fa-remove');
+            addedFriendList.appendChild(document.getElementById(item));
+        }
+    })
